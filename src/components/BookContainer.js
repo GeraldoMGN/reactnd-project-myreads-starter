f3ef9e77cea7a421eee21bec9dcdd9423837c931
unknown <gmgn@cin.ufpe.br>
2020-01-26 11:59:51 -0300

    refactor: Extract Book and add components
    Extract Book from Bookshelf, add BookContainer to separate logic and
    extract BookshelfSelector from Book for reusability

---
+++ src/components/BookContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//TODO: This will be responsable for updating the books
+export default class BookContainer extends React.Component {
+  render() {
+    <Book />
+  }
+}
