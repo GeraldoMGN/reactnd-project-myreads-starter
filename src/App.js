85924ed2ce1025fd070bebd7fa69719c2ef107e6
unknown <gmgn@cin.ufpe.br>
2020-01-26 10:14:23 -0300

    refactor: Moved ListBooksHeader to  it's own file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 // import * as BooksAPI from './BooksAPI'
-import './App.css'
+import './App.css';
+import ListBooksHeader from './components/ListBooksHeader';
 
 class BooksApp extends React.Component {
   state = {
@@ -39,9 +40,7 @@ class BooksApp extends React.Component {
           </div>
         ) : (
           <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+            <ListBooksHeader />
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
---
+++ src/components/ListBooksHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div className="list-books-title">
+    <h1>MyReads</h1>
+  </div>
+);
