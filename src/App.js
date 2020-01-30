23070047b86344e1e53d31691fa7fda1b890c484
Geraldo Galvao <gmgn@cin.ufpe.br>
2020-01-30 14:05:35 -0300

    refactor: Lifted the books array to App component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import SearchPage from './components/SearchPage';
 import ShelvesPage from './components/ShelvesPage';
 import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 
 export default class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  updateBooks = () => BooksAPI.getAll()
+    .then(books => { this.setState({
+        books,
+        loaded: true
+      })
+    });
+
+  componentDidMount = () => this.updateBooks();
+
   render() {
     return (
       <div className="app">
         <Route exact path="/search" component={SearchPage} />
-        <Route exact path="/" component={ShelvesPage} />
+        <Route exact path="/" render={() => 
+          <ShelvesPage books={this.state.books} updateBooks={this.updateBooks} />}
+        />
       </div>
     );
   }
--- src/components/ShelvesPage.js
+++ src/components/ShelvesPage.js
@@ -2,22 +2,11 @@ import React from 'react';
 import ListBooksHeader from './ListBooksHeader';
 import { Link } from 'react-router-dom';
 import BookShelf from './Bookshelf';
-import * as BooksAPI from '../BooksAPI';
 
 export default class ShelvesPage extends React.Component {
   state = {
-    books: [],
     loaded: false
   }
-
-  updateBooks = () => BooksAPI.getAll()
-    .then(books => { this.setState({
-        books,
-        loaded: true
-      })
-    });
-
-  componentDidMount = () => this.updateBooks();
     
   categorizeBooks = books => books.reduce((categorizedBooks, book) => {
       categorizedBooks[book.shelf] = (categorizedBooks[book.shelf] || []).concat(book);
@@ -25,7 +14,7 @@ export default class ShelvesPage extends React.Component {
   }, {});
 
   render() {
-    const {currentlyReading = [], wantToRead = [], read = []} = this.categorizeBooks(this.state.books);
+    const {currentlyReading = [], wantToRead = [], read = []} = this.categorizeBooks(this.props.books);
 
     return (
       <div className="list-books">
@@ -33,15 +22,15 @@ export default class ShelvesPage extends React.Component {
         <div className="list-books-content">
           <BookShelf title='Currently Reading' 
                      books={currentlyReading} 
-                     updateBooks={this.updateBooks} 
+                     updateBooks={this.props.updateBooks} 
           />
           <BookShelf title='Want to Read' 
                      books={wantToRead} 
-                     updateBooks={this.updateBooks} 
+                     updateBooks={this.props.updateBooks} 
           />
           <BookShelf title='Read' 
                      books={read} 
-                     updateBooks={this.updateBooks} 
+                     updateBooks={this.props.updateBooks} 
           />
         </div>
         <Link className="open-search" to="/search">
